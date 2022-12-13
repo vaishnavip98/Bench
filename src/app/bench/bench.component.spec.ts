243513f967658088ed13172e87bfaa2bc2a0c6fd
Vaishnavi P <vaishnavi.p@flatworldlocal.com>
2022-12-13 17:45:28 +0530

    first commit

---
+++ src/app/bench/bench.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BenchComponent } from './bench.component';
+
+describe('BenchComponent', () => {
+  let component: BenchComponent;
+  let fixture: ComponentFixture<BenchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BenchComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BenchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/bench/bench.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bench',
+  templateUrl: './bench.component.html',
+  styleUrls: ['./bench.component.css']
+})
+export class BenchComponent {
+
+}
